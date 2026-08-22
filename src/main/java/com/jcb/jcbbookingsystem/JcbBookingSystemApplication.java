cb05dab6678dcb44d9e82170f475bf77965bb121
https://github.com/IT25100045/test.git <it25100045@my.sliit.lk>
2026-08-23 00:02:49 +0530

    Initial Spring Boot project setup with MySQL config

---
+++ src/main/java/com/jcb/jcbbookingsystem/JcbBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jcb.jcbbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcbBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcbBookingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jcb/jcbbookingsystem/JcbBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcb.jcbbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcbBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
