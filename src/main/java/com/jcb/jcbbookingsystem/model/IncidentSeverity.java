c6ff194a69caede76c73b8c8fa9ba04557a33b86
IT25100049 <it25100049@my.sliit.lk>
2026-09-15 20:49:46 +0530

    add incident report model and enums

---
+++ src/main/java/com/jcb/jcbbookingsystem/model/IncidentSeverity.java
@@ -0,0 +1,9 @@
+package com.jcb.jcbbookingsystem.model;
+
+public enum IncidentSeverity {
+    LOW,
+    MEDIUM,
+    HIGH,
+    CRITICAL
+
+}
---
+++ src/main/java/com/jcb/jcbbookingsystem/model/IncidentStatus.java
@@ -0,0 +1,7 @@
+package com.jcb.jcbbookingsystem.model;
+
+public enum IncidentStatus {
+    OPEN,
+    IN_PROGRESS,
+    RESOLVED
+}
